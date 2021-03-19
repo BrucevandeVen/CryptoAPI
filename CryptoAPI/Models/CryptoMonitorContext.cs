62db086ba152b194bcc448d0422317ddcdd4ec95
Bruce van de Ven <430690@student.fontys.nl>
2021-03-19 17:12:23 +0100

    Connection string verplaatst naar appsettings.json

--- CryptoAPI/Models/CryptoMonitorContext.cs
+++ CryptoAPI/Models/CryptoMonitorContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -21,11 +22,11 @@ namespace CryptoAPI.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(LocalDb)\\DBServer;Database=CryptoMonitor;Trusted_Connection=True;");
-            }
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("CryptoString"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
