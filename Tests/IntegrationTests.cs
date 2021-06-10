4211b0a0ae6a40e410941ac530028018916381b4
Bruce van de Ven <430690@student.fontys.nl>
2021-06-10 14:03:10 +0200

    integrationtests

---
+++ Tests/IntegrationTests.cs
@@ -0,0 +1,20 @@
+﻿using CryptoAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests
+{
+    public class IntegrationTests
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTests()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            TestClient = factory.CreateClient();
+        }
+    }
+}
